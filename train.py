1aa418a4ea87c7f658988e068c353df5cc774a1b
Matthew Taylor <mjt@ku.edu>
2019-03-26 12:09:15 -0500

    Slight file restructure

--- train.py
+++ train.py
@@ -1,10 +1,35 @@
 import pickle
+import nltk
 from hmm import HMM
+import sys
+import string
 
-# get clean, preformatted text from corpus
-print('Loading sample text... ', end='')
-text = pickle.load(open('pickle/sentences.p', 'rb'))
+print('Cleaning text... ', end='')
+sys.stdout.flush()
+# get raw text from file
+f = open('text/amazon_reviews.txt')
+text = ' '.join(f.read().splitlines())
+f.close()
+
+# split text into sentences
+sentences = nltk.sent_tokenize(text)
+
+lines_to_remove = []
+for i in range(len(sentences)):
+
+	# remove all punctuation and set all words to lowercase
+	sentences[i] = sentences[i].translate(str.maketrans('', '', string.punctuation)).lower()
+
+	# record which lines are blank, so those can be removed
+	if sentences[i] == '':
+		lines_to_remove = [i] + lines_to_remove
+
+# remove blank lines
+for i in lines_to_remove:
+	del sentences[i]
 print('done')
+sys.stdout.flush()
 
+# train model
 model = HMM(n_hidden_states=8)
-model.train(text)
+model.train(sentences)
